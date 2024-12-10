612739e66689046ec2bd73817c1a2c48195e37bc
EMIYAID04 <sulthanza06@gmail.com>
2024-12-10 12:25:13 +0700

    Resolve conflict

--- app/src/main/java/com/example/schoolimpact/ui/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/example/schoolimpact/ui/onboarding/OnboardingViewModel.kt
@@ -21,15 +21,9 @@ class OnboardingViewModel @Inject constructor() : ViewModel() {
 
     private fun loadOnboardingItems() {
         _onboardingItem.value = listOf(
-<<<<<<< HEAD
-            OnboardingItem(R.drawable.onboarding_image1, " Rekomendasi Sekolah", "Description 1"),
-            OnboardingItem(R.drawable.onboarding_image2, "Jurusan Sekolah Tingkat Atas", "Description 2"),
-            OnboardingItem(R.drawable.onboarding_image3, "Profil", "Description 3")
-=======
             OnboardingItem(R.drawable.onboarding_image1, "Kenali Potensimu!", "Jelajahi bakat dan minatmu untuk memilih jurusan yang terbaik."),
             OnboardingItem(R.drawable.onboarding_image2, "Pilihan Tepat Untuk Masa Depan Cerah", "Dapatkan rekomendasi jurusan berdasarkan penilaian dan preferensimu."),
             OnboardingItem(R.drawable.onboarding_image3, "Siap Melangkah?", "Awali perjalananmu menuju masa depan yang gemilang.")
->>>>>>> master
         )
     }
 }
