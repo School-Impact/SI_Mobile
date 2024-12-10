aff39cc8e1128045472e5489d834bbb46f00ff29
EMIYAID04 <sulthanza06@gmail.com>
2024-12-10 13:53:10 +0700

    implement dark mode

--- app/src/main/java/com/example/schoolimpact/MainActivity.kt
+++ app/src/main/java/com/example/schoolimpact/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.schoolimpact
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.schoolimpact.databinding.ActivityMainBinding
+import com.example.schoolimpact.ui.main.profile.ProfileViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -18,6 +23,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var navController: NavController
+
+    private val viewModel: ProfileViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,20 +42,24 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         navController = navHostFragment.navController
 
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_home, R.id.navigation_discover, R.id.navigation_profile
-//            )
-//        )
-
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.navigation_detail_major -> navView.visibility = View.GONE
                 else -> navView.visibility = View.VISIBLE
             }
         }
-//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.getThemeSettings().collect { isDarkModeActive ->
+                    AppCompatDelegate.setDefaultNightMode(
+                        if (isDarkModeActive) AppCompatDelegate.MODE_NIGHT_YES
+                        else AppCompatDelegate.MODE_NIGHT_NO
+                    )
+                }
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/example/schoolimpact/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/schoolimpact/ui/main/profile/ProfileFragment.kt
@@ -2,9 +2,11 @@ package com.example.schoolimpact.ui.main.profile
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -32,7 +34,9 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val contextThemeWrapper = ContextThemeWrapper(requireContext(), R.style.Theme_SchoolImpact)
+        val localInflater = inflater.cloneInContext(contextThemeWrapper)
+        _binding = FragmentProfileBinding.inflate(localInflater, container, false)
         return binding.root
     }
 
@@ -42,8 +46,12 @@ class ProfileFragment : Fragment() {
         setupObservers()
         viewModel.getUserProfile()
 
-        binding.btnLogout.setOnClickListener { logout() }
 
+        binding.switchTheme.setOnCheckedChangeListener { _, isChecked ->
+            viewModel.saveThemeSetting(isChecked)
+        }
+
+        binding.btnLogout.setOnClickListener { logout() }
     }
 
     private fun setupObservers() {
@@ -74,6 +82,20 @@ class ProfileFragment : Fragment() {
                         }
 
                         else -> Unit
+                    }
+                }
+            }
+        }
+        // Switch theme
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.getThemeSettings().collect { isDarkModeActive ->
+                    if (isDarkModeActive) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        binding.switchTheme.isChecked = true
+                    } else {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        binding.switchTheme.isChecked = false
                     }
                 }
             }
